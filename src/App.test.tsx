6d6e342dae46afd3c7777344d3057221ebf5f638
just_vovka <vovkamokroff@gmail.com>
2021-09-29 22:32:21 +0300

    add eslint configs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,20 @@
+
+import { ShallowWrapper, configure, shallow } from 'enzyme';
 import React from 'react';
-import { configure, shallow, ShallowWrapper } from 'enzyme';
+
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
 describe('<App>', () => {
-  let wrapper:ShallowWrapper<any, React.Component['state'], React.Component>;
+  let wrapper:ShallowWrapper<JSX.Element, React.Component['state'], React.Component>;
 
   beforeEach(() => {
-    configure({adapter: new Adapter()});
+    configure({ adapter: new Adapter() });
     wrapper = shallow(<App />);
 
   });
   test('App component should be render', () => {
-    expect(wrapper.text()).toEqual('Application');
+    expect(wrapper.text()).toEqual('Application CRUD');
   });
 });
